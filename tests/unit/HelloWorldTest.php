11c8e2b56523504342e60662b731a72405c7e239
Leonardo Tumadjian <tumadjian@gmail.com>
2019-06-06 16:38:52 -0300

    Drone teste 2

---
+++ tests/unit/HelloWorldTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testCheckFirstTestOnDrone()
+    {
+        $out = 'Hello World';
+
+        $this->assertEquals('Hello world', $out);
+    }
+}
